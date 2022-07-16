95c024c0cd76eae71c5ec78bbf18737ce3f1dedf
Knot <akthakorn@gmail.com>
2022-07-16 16:13:48 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <p>Todo id: {{ todoId }}</p>
+  <button @click="todoId++">Fetch next todo</button>
+  <li v-for="(item) in todoData" :key="item">
+    {{ item.Name }}
+  </li> -->
+  <div>
+    ใส่น้ำหนัก
+    <input v-model="weight" />
+    <br />
+    ใส่ส่วนสูง
+    <input v-model="height" />
+
+    <br />
+    <button @click="getBmi">คำนวณ BMI</button>
+    <br />
+    <br />
+    {{ resBmi }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+
+  data() {
+    return {
+      todoId: 1,
+      todoData: null,
+      resBmi: null,
+      height: 0,
+      weight: 0,
+    };
+  },
+
+  methods: {
+    async fetchData() {
+      this.todoData = null;
+      const res = await fetch(`http://127.0.0.1:8000/`);
+      this.todoData = await res.json();
+    },
+    async getBmi() {
+      const res = await fetch(
+        `http://127.0.0.1:8000/get_bmi/${this.height}-${this.weight}`
+      );
+      this.resBmi = await res.json();
+    },
+  },
+
+  mounted() {
+    this.fetchData();
+  },
+
+  watch: {
+    todoId() {
+      this.fetchData();
+    },
+  },
+};
 </script>
 
 <style>
